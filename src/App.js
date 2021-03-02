4bbcfc7bc699f84e02c9e123830865e0ed636a71
lu1151 <luxi.li@outlook.com>
2021-03-02 00:23:55 -0500

    03/02/2021

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: "Adidas",
+    src:
+      "https://corunco.com/wp-content/uploads/2016/10/adidas-black-vector-logo-400x400.png"
+  },
+  {
+    title: "Starbucks",
+    src:
+      "https://logodownload.org/wp-content/uploads/2017/10/starbucks-logo-5.png"
+  },
+  {
+    title: "Nike",
+    src:
+      "https://www.designwizard.com/wp-content/uploads/2019/10/Nike-Logo.png"
+  },
+]
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card {...list[0]} />
     </div>
   );
+}
+
+function DraggableList({ list }) {
+  return (
+    <div className="draggable-list">
+
+    </div>
+  )
+}
+
+function Draggable({ children }) {
+  return (
+    <div className="draggable">{children}</div>
+  )
+}
+
+function Bar() {
+
+}
+
+function Card({ title, src }) {
+  return (
+    <div className="card">
+      <img src={src} alt={title} />
+      <span>{title}</span>
+    </div>
+  )
 }
 
 export default App;
